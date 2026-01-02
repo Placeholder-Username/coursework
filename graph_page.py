aa315c66af66a9cd81a58a366060646c8864b32a
artysun <19aakinola@uobschool.org.uk>
2026-01-02 14:29:47 +0000

    making the graph Gui

--- graph_page.py
+++ graph_page.py
@@ -1,17 +1,11 @@
-import tkinter as tk
-class MyGUI:
-    # this is the main window of the app
-    def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("PomoFocus")
-        self.window.geometry("800x600")
-        self.window.configure(bg="white")
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.dates import DAYS_PER_WEEK
 
-        # main title for the page
-        self.label = tk.Label(self.window, text="Graph page", font=("Arial", 18))
-        self.label.pack(pady=20)
+days = (["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"])
+values = ([])
+for day in days:
+    values.append(day)
 
-        # starts the GUI
-        self.window.mainloop()
-
-MyGUI()
+plt.bar(days,values)
+plt.show()
