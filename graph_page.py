bb7d2de5b6fec939404b534ab0ee65990e00c053
artysun <19aakinola@uobschool.org.uk>
2026-01-02 14:48:08 +0000

    making the graph Gui

--- graph_page.py
+++ graph_page.py
@@ -1,11 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.dates import DAYS_PER_WEEK
 
-days = (["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"])
-values = ([])
-for day in days:
-    values.append(day)
-
-plt.bar(days,values)
+days = ["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"]
+minutes_studied = [0,30,45,60,90,99]
+colours = []
+for minutes in minutes_studied:
+    if minutes == 0:
+        colours.append("red")
+    elif minutes < 30:
+        colours.append("orange")
+    else:
+        colours.append("green")
+plt.bar(days,minutes_studied,color = colours)
+plt.title("Graph of Sessions")
+plt.xlabel("Days")
+plt.ylabel("Study Time")
 plt.show()
