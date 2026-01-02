d8a0dc2b72bd07c002bd91cdd658797963023763
artysun <19aakinola@uobschool.org.uk>
2026-01-02 17:57:53 +0000

    just made a bar graph hope it works with tkinter

--- graph_page.py
+++ graph_page.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-days = ["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"]
-minutes_studied = [0,30,45,60,90,99]
-colours = []
-for minutes in minutes_studied:
-    if minutes == 0:
-        colours.append("red")
-    elif minutes < 30:
-        colours.append("orange")
-    else:
-        colours.append("green")
-plt.bar(days,minutes_studied,color = colours)
-plt.title("Graph of Sessions")
-plt.xlabel("Days")
-plt.ylabel("Study Time")
-plt.show()
+def Show_graph(self):
+    days = ["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"]
+    minutes_studied = [0,30,45,60,90,99]
+    colours = []
+    for minutes in minutes_studied:
+        if minutes == 0:
+            colours.append("red")
+        elif minutes < 30:
+            colours.append("orange")
+        else:
+            colours.append("green")
+    plt.bar(days,minutes_studied,color = colours)
+    plt.title("Graph of Sessions")
+    plt.xlabel("Days")
+    plt.ylabel("Study Time")
+    plt.show()
